53ccf268a8c0bb3a1f2ba365d470221315dc0c81
Alexandr <jwchaganav@gmail.com>
2023-12-01 15:25:05 +0400

    arrays

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -1,5 +1,15 @@
 public class Program {
     public static void main(String[] args) {
         System.out.println("Reflection API");
+        System.out.println();
+        Animal[] animals = new Animal[]{
+                new Dog("Лорд",2),
+                new Dog("Барбос", 4),
+                new Cat("Мурка",1),
+                new Cat("Рыжик",3)
+        };
+        for (Animal item: animals) {
+            item.toString();
+        }
     }
 }
