222898fc649c025579e8982d3f12a70ee5f1f512
Alexandr <jwchaganav@gmail.com>
2023-12-05 12:04:21 +0400

    запустил makeSound

--- src/main/java/Dog.java
+++ src/main/java/Dog.java
@@ -1,4 +1,5 @@
 public class Dog extends Animal{
+    private String breed;
     public Dog(String name, int age) {
         super(name, age);
     }
@@ -11,5 +12,13 @@ public class Dog extends Animal{
     @Override
     public String toString() {
         return String.format("$s $s",this.getClass(),super.toString());
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
     }
 }
--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -1,11 +1,12 @@
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 import static java.lang.reflect.Method.*;
 
 public class Program {
-    public static void main(String[] args) throws ClassNotFoundException {
+    public static void main(String[] args) throws ClassNotFoundException, InvocationTargetException, IllegalAccessException, InstantiationException {
         System.out.println("Reflection API");
         System.out.println();
         Animal[] animals = new Animal[]{
@@ -17,16 +18,18 @@ public class Program {
         for (Animal animal: animals) {
             Class<?> any = Class.forName(animal.getClass().getName());
             System.out.println(String.format("%s %s",any.getName(),animal.getName()));
+            String anyName = animal.getName();
             Constructor<?>[] constructors = any.getConstructors();
             System.out.println("Конструкторы");
             for (int i = 0; i < constructors.length; i++) {
                 System.out.println(constructors[i]);
             }
-            Field[] fields = any.getFields();
+            Field[] fields = any.getDeclaredFields();
             System.out.println("Поля");
             for (int i = 0; i < fields.length; i++) {
                 System.out.println(fields[i]);
             }
+
             Method[] methods = any.getDeclaredMethods();
             System.out.println("Методы");
             for (int i = 0; i < methods.length; i++) {
@@ -34,9 +37,10 @@ public class Program {
                 if(methods[i].getName().equals("makeSound")){
                     System.out.println(methods[i].getName());
                     Method method = methods[i];
-
+                    method.invoke(animal);
                 }
             }
+            System.out.println("========");
         }
     }
 }
